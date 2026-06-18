e62d89c3f8e6fa06f6a185e1cc6f460ed2a8f1f6
hayne-jeong <hayne-jeong@users.noreply.github.com>
2026-06-19 00:10:45 +0900

    Add romaji to word list

--- app.js
+++ app.js
@@ -153,6 +153,74 @@ function selectJapaneseVoice() {
   return scoredVoices[0].voice;
 }
 
+function kanaToRomaji(input) {
+  const kana = normalizeKana(input);
+  const base = {
+    あ: "a", い: "i", う: "u", え: "e", お: "o",
+    か: "ka", き: "ki", く: "ku", け: "ke", こ: "ko",
+    が: "ga", ぎ: "gi", ぐ: "gu", げ: "ge", ご: "go",
+    さ: "sa", し: "shi", す: "su", せ: "se", そ: "so",
+    ざ: "za", じ: "ji", ず: "zu", ぜ: "ze", ぞ: "zo",
+    た: "ta", ち: "chi", つ: "tsu", て: "te", と: "to",
+    だ: "da", ぢ: "ji", づ: "zu", で: "de", ど: "do",
+    な: "na", に: "ni", ぬ: "nu", ね: "ne", の: "no",
+    は: "ha", ひ: "hi", ふ: "fu", へ: "he", ほ: "ho",
+    ば: "ba", び: "bi", ぶ: "bu", べ: "be", ぼ: "bo",
+    ぱ: "pa", ぴ: "pi", ぷ: "pu", ぺ: "pe", ぽ: "po",
+    ま: "ma", み: "mi", む: "mu", め: "me", も: "mo",
+    や: "ya", ゆ: "yu", よ: "yo",
+    ら: "ra", り: "ri", る: "ru", れ: "re", ろ: "ro",
+    わ: "wa", を: "o", ん: "n",
+    ゔ: "vu", ぁ: "a", ぃ: "i", ぅ: "u", ぇ: "e", ぉ: "o",
+  };
+  const combos = {
+    きゃ: "kya", きゅ: "kyu", きょ: "kyo",
+    ぎゃ: "gya", ぎゅ: "gyu", ぎょ: "gyo",
+    しゃ: "sha", しゅ: "shu", しょ: "sho",
+    じゃ: "ja", じゅ: "ju", じょ: "jo",
+    ちゃ: "cha", ちゅ: "chu", ちょ: "cho",
+    にゃ: "nya", にゅ: "nyu", にょ: "nyo",
+    ひゃ: "hya", ひゅ: "hyu", ひょ: "hyo",
+    びゃ: "bya", びゅ: "byu", びょ: "byo",
+    ぴゃ: "pya", ぴゅ: "pyu", ぴょ: "pyo",
+    みゃ: "mya", みゅ: "myu", みょ: "myo",
+    りゃ: "rya", りゅ: "ryu", りょ: "ryo",
+  };
+
+  let result = "";
+  for (let index = 0; index < kana.length; index += 1) {
+    const char = kana[index];
+    const next = kana[index + 1];
+    const pair = char + next;
+
+    if (char === "っ") {
+      const nextRomaji = combos[pair] || base[next] || "";
+      result += nextRomaji.match(/^[bcdfghjklmnpqrstvwxyz]/)?.[0] || "";
+      continue;
+    }
+
+    if (char === "ー") {
+      const vowel = result.match(/[aeiou](?!.*[aeiou])/)?.[0] || "";
+      result += vowel;
+      continue;
+    }
+
+    if (combos[pair]) {
+      result += combos[pair];
+      index += 1;
+      continue;
+    }
+
+    result += base[char] ?? char;
+  }
+
+  return result;
+}
+
+function normalizeKana(input) {
+  return input.replace(/[ァ-ヶ]/g, (char) => String.fromCharCode(char.charCodeAt(0) - 0x60));
+}
+
 function renderSelectors() {
   if (els.daySelect) {
     els.daySelect.innerHTML = "";
@@ -210,7 +278,10 @@ function renderList() {
       button.innerHTML = `
         <span class="list-no">${String(card.no).padStart(2, "0")}</span>
         <span class="list-word">${card.kanji}</span>
-        <span class="list-kana">${card.kana}</span>
+        <span class="list-reading">
+          <span class="list-kana">${card.kana}</span>
+          <span class="list-romaji">${kanaToRomaji(card.kana)}</span>
+        </span>
         <span class="list-meaning">${card.meaning}</span>
       `;
       button.addEventListener("click", () => {
